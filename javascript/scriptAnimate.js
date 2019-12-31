1f0b67a83a6977ce037e1f366e1254615aa9468d
Katarina2 <katarina.galdino@dellead.com>
2019-12-31 17:30:07 -0300

    Initial commit

---
+++ javascript/scriptAnimate.js
@@ -0,0 +1,48 @@
+// Debounce do Lodash
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+
+(function(){
+	var $target = $('.animated'),
+			offset = $(window).height() * 3/4;
+
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
+
+		$target.each(function(){
+			var itemTop = $(this).offset().top;
+			if (documentTop > itemTop - offset) {
+				if($(this).hasClass("appearLeft")){
+					$(this).addClass("ativo slideInLeft");
+				}
+				else if($(this).hasClass("appearRight")){
+					$(this).addClass("ativo slideInRight");
+				}
+				else if($(this).hasClass("appearGrowingUp")){
+					$(this).addClass("ativo zoomIn");
+				}
+			
+			}			
+		});
+	}
+
+	animeScroll();
+
+	$(document).scroll(debounce(function(){
+		animeScroll();
+	}, 200));
+})();
+
